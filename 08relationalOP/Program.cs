612e10a77c2d42cb41a97d1eff76089c514d0380
João Guilherme Oliani <jguilherme.oliani@gmail.com>
2025-07-17 09:09:58 -0300

    add part 08

--- 08relationalOP/Program.cs
+++ 08relationalOP/Program.cs
@@ -2,13 +2,14 @@
 int number2 = 6;
 
 //bigger
-
+Console.WriteLine(number1 > number2);
 //bigger or equal
-
+Console.WriteLine(number1 >= number2);
 //smaller
-
+Console.WriteLine(number1 < number2);
 //smaller or equal
-
+Console.WriteLine(number1 <= number2);
 //equal
-
+Console.WriteLine(number1 == number2);
 //not equal
+Console.WriteLine(number1 != number2);
